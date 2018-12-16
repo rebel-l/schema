ef11a7330c162a4d3bb204ea37340555931d89c4
Rebel-L <dj@rebel-l.net>
2018-12-16 03:12:23 +0100

    added database structure with responsible struct

---
+++ store/schema_version.go
@@ -0,0 +1,40 @@
+// Package store is responsible to persist the data of schema package
+package store
+
+import "time"
+
+const (
+	// StatusSuccess is the status name for 'success'
+	StatusSuccess = "success"
+
+	// StatusError is the status name for 'error'
+	StatusError = "error"
+)
+
+// SchemaVersion represents the version information stored in the database
+type SchemaVersion struct {
+	ID         int32     `db:"id"`
+	ScriptName string    `db:"script_name"`
+	ExecutedAt time.Time `db:"executed_at"`
+	Status     string    `db:"execution_status"`
+	ErrorMsg   string    `db:"error_msg"`
+}
+
+// NewSchemaVersionSuccess returns a new SchemaVersion struct prepared for successful execution
+func NewSchemaVersionSuccess(scriptName string) *SchemaVersion {
+	return &SchemaVersion{
+		ScriptName: scriptName,
+		ExecutedAt: time.Now(),
+		Status:     StatusSuccess,
+	}
+}
+
+// NewSchemaVersionError returns a new SchemaVersion struct prepared for failed execution
+func NewSchemaVersionError(scriptName string, errorMsg string) *SchemaVersion {
+	return &SchemaVersion{
+		ScriptName: scriptName,
+		ExecutedAt: time.Now(),
+		Status:     StatusError,
+		ErrorMsg:   errorMsg,
+	}
+}
