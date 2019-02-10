d0d992c3fc5025af29a3187b471571a6967aec6a
Lars Gaubisch <l.gaubisch@googlemail.com>
2019-02-10 22:50:02 +0100

    switched coverage to reports path and fixed IDE warnings on script

--- scripts/tools/runChecks.sh
+++ scripts/tools/runChecks.sh
@@ -7,28 +7,28 @@ echo
 echo -en "\E[40;35m\033[1mExecute linters\033[0m"
 echo
 gometalinter --config=gometalinter.json
-EXITCODE=$?
-if [ $EXITCODE != 0 ]
+EXIT_CODE=$?
+if [[ ${EXIT_CODE} != 0 ]]
 then
 	echo
-	echo -en "\E[40;31m\033[1mLinting failed with exit code: \033[0m" $EXITCODE
+	echo -en "\E[40;31m\033[1mLinting failed with exit code: \033[0m" ${EXIT_CODE}
 	echo
 	echo
-	exit $EXITCODE
+	exit ${EXIT_CODE}
 fi
 
 # Execute tests
 echo -en "\E[40;35m\033[1mExecute tests\033[0m"
 echo
-go test -v ./...
-EXITCODE=$?
-if [ $EXITCODE != 0 ]
+go test -v -cover -race -coverprofile=./reports/coverage.txt -covermode=atomic ./...
+EXIT_CODE=$?
+if [[ ${EXIT_CODE} != 0 ]]
 then
 	echo
-	echo -en "\E[40;31m\033[1mTests failed with exit code: \033[0m" $EXITCODE
+	echo -en "\E[40;31m\033[1mTests failed with exit code: \033[0m" ${EXIT_CODE}
 	echo
 	echo
-	exit $EXITCODE
+	exit ${EXIT_CODE}
 fi
 
 # Success
